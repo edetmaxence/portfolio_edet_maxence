e53599dcf8c45f8dc18d08def40671ce0caa7c8e
edetmaxence <edet.maxence@gmail.com>
2023-01-13 19:42:09 +0100

    Etat de base

--- accueil.php
+++ accueil.php
@@ -149,11 +149,14 @@ $apropos = get_field("apropos");
 
 
 <section>
-<div class="div2" id="realisations">
-		<div class="container">
-			<h1>Mes Réalisations - Projets</h1>
+	<div id="realisations">
+		<div class="container-fluid">
+			<div class="row text-center">
+				<span class="color_gold display-4">Mes Réalisations - Projets</span>
+			</div>
+			
 			<!-- groupe bouton --->
-			<div class="row">
+			<div class="row text-center">
 				<div class="col-md-9 col-sm-10 offset-1">
 
 					<div class="button-group filter-button-group" data-filter-group="categoriesrealisation">
@@ -161,36 +164,36 @@ $apropos = get_field("apropos");
 						<ul class="cat_filter" style="padding-top: 4rem;">
 
 							<!--btn tous-->
-							<a class="filter-button redlink" id="filter-all">
+							<a class=" redlink" id="filter-all">
 								<svg width="80px" height="80px" xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-code" viewBox="0 0 16 16">
 									<path d="M5.854 4.854a.5.5 0 1 0-.708-.708l-3.5 3.5a.5.5 0 0 0 0 .708l3.5 3.5a.5.5 0 0 0 .708-.708L2.707 8l3.147-3.146zm4.292 0a.5.5 0 0 1 .708-.708l3.5 3.5a.5.5 0 0 1 0 .708l-3.5 3.5a.5.5 0 0 1-.708-.708L13.293 8l-3.147-3.146z" />
 								</svg>
 
-								<span class="btnpurple">TOUS</span>
+								<span class="fw-bolder">TOUS</span>
 							</a>
 							<!-- btn mobile--->
-							<a class="filter-button redlink" id="filter-mobile">
+							<a class=" redlink" id="filter-mobile">
 								<svg width="80px" height="80px" xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-phone" viewBox="0 0 16 16">
 									<path d="M11 1a1 1 0 0 1 1 1v12a1 1 0 0 1-1 1H5a1 1 0 0 1-1-1V2a1 1 0 0 1 1-1h6zM5 0a2 2 0 0 0-2 2v12a2 2 0 0 0 2 2h6a2 2 0 0 0 2-2V2a2 2 0 0 0-2-2H5z" />
 									<path d="M8 14a1 1 0 1 0 0-2 1 1 0 0 0 0 2z" />
 								</svg>
-								<span class="btnpurple">MOBILE</span>
+								<span class="fw-bolder">MOBILE</span>
 							</a>
 
 							<!-- btn web--->
-							<a class="filter-button redlink" id="filter-web">
+							<a class=" redlink" id="filter-web">
 								<svg width="80px" height="80px" xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-window" viewBox="0 0 16 16">
 									<path d="M2.5 4a.5.5 0 1 0 0-1 .5.5 0 0 0 0 1zm2-.5a.5.5 0 1 1-1 0 .5.5 0 0 1 1 0zm1 .5a.5.5 0 1 0 0-1 .5.5 0 0 0 0 1z" />
 									<path d="M2 1a2 2 0 0 0-2 2v10a2 2 0 0 0 2 2h12a2 2 0 0 0 2-2V3a2 2 0 0 0-2-2H2zm13 2v2H1V3a1 1 0 0 1 1-1h12a1 1 0 0 1 1 1zM2 14a1 1 0 0 1-1-1V6h14v7a1 1 0 0 1-1 1H2z" />
 								</svg>
-								<span class="btnpurple">WEB</span>
+								<span class="fw-bolder">WEB</span>
 							</a>
 							<!-- btn autre--->
-							<a class="filter-button redlink" id="filter-autre">
+							<a class=" redlink" id="filter-autre">
 								<svg width="80px" height="80px" xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-code-slash" viewBox="0 0 16 16">
 									<path d="M10.478 1.647a.5.5 0 1 0-.956-.294l-4 13a.5.5 0 0 0 .956.294l4-13zM4.854 4.146a.5.5 0 0 1 0 .708L1.707 8l3.147 3.146a.5.5 0 0 1-.708.708l-3.5-3.5a.5.5 0 0 1 0-.708l3.5-3.5a.5.5 0 0 1 .708 0zm6.292 0a.5.5 0 0 0 0 .708L14.293 8l-3.147 3.146a.5.5 0 0 0 .708.708l3.5-3.5a.5.5 0 0 0 0-.708l-3.5-3.5a.5.5 0 0 0-.708 0z" />
 								</svg>
-								<span class="btnpurple">AUTRE</span>
+								<span class="fw-bolder">AUTRE</span>
 							</a>
 
 						</ul>
@@ -214,9 +217,10 @@ $apropos = get_field("apropos");
 					?>
 
 							<div class="grid-item  col-md-4 m-3
-                          <?php
-							istop_classes(get_the_id());
-							?>" href="<?php the_permalink(); ?>">
+								<?php
+									istop_classes(get_the_id());
+									?>" href="<?php the_permalink(); ?>
+							">
 
 								<?php if (!empty($img)) : ?>
 									<img src=" <?= $img ?>">
